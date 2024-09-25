08c3cd26c60a60a5c3df9b4456a0734d8bf0efc1
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-09-25 15:56:47 -0300

    Operadores aritméticos JS

---
+++ fundamentos/exercicios/inicio/26-oper-aritmetico.js
@@ -0,0 +1,15 @@
+//Operadores aritméticos são operadores binários
+const [a, b, c, d] = [3, 5, 1, 15]
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplacacao = a * b
+const divisao = d / a
+const modulo = a % 2 //Resto da divisão
+console.log(soma, subtracao, multiplacacao, divisao, modulo)
+
+
+// '+' é um operador binario
+// ++ >  operador/incremento postfix
+// ++d > prefix
+//op1 + op2  (infix)
+// '-' é um operador unário
