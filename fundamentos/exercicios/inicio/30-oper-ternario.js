27b936471f3322b49e522ffc1b437f7bdd6f5c12
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-09-26 23:59:30 -0300

    Operador ternário JS #parte-2

--- fundamentos/exercicios/inicio/30-oper-ternario.js
+++ fundamentos/exercicios/inicio/30-oper-ternario.js
@@ -1,6 +1,14 @@
 //Funciona como uma estrutura condicional, resumida em 3 partes (condição ? expr1 : expr2)
 
-const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+/* const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' */
+
+//Ou
+
+const resultado = nota => {
+    return nota >= 7
+    ? 'Aprovado'
+    : 'Reprovado'
+} 
 
 console.log(resultado(8))
 console.log(resultado(6.99))
