66199deeaf34d43131fa429422045887483dee60
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-06 00:11:48 -0300

    Cidadão primeira classe JS

--- função/1-cidadao-de-primeira-linha.js
+++ função/1-cidadao-de-primeira-linha.js
@@ -6,4 +6,33 @@ function fun1() {
     //Se não for declarada, ela retorna undefined
 }
 
-console.log(typeof fun1())
+//Armazenar em uma variável
+const fun2 = function() {} 
+
+//Armazenar em um array
+const array = [function(a, b) {return a + b}, fun1, fun2]
+
+console.log(array[0](2, 3))
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() {return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como parametro
+function run(fun) {
+    fun()
+}
+
+run(function() {console.log('Executando...')})
+
+//Uma função pode retornar/conter uma função
+function soma(a, b) {
+    return function(c) {
+        console.log(a + b + c)
+    }
+}
+
+soma(2, 3)(4) //9
+const cincoMais = soma(2, 3)
+cincoMais(4)
