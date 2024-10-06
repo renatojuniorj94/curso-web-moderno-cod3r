982cf1f2985e5923ac42d0c4107d6e771fc8f3ac
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-05 23:52:13 -0300

    Cidadão primeira classe JS

---
+++ função/1-cidadao-de-primeira-linha.js
@@ -0,0 +1,9 @@
+//Função em JS é first-class object (Citizens)
+//Higher-order function > Função de alta ordem
+
+//Criando função de forma literal
+function fun1() {
+    //Se não for declarada, ela retorna undefined
+}
+
+console.log(typeof fun1())
