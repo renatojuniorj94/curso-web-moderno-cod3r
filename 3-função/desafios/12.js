309e027e1ed46cc7742966cf932ca03d063af6cb
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-25 13:59:16 -0300

    Exercício 12 (parte 2)

--- 3-função/desafios/12.js
+++ 3-função/desafios/12.js
@@ -2,14 +2,16 @@
 
 function fatorial(num) {
     let texto = `!${num}`
+    let fat = num
     if (num == 0 || num == 1) {
-        return 1
+        return '!' + 1
     } else if (num >= 2) {
         for (let c = num; --c; c >= 1) {
             texto += ` x ${c}`
+            fat *= c
         }
     }
-    return texto
+    return texto + ' = ' + fat
 }
 
 console.log(fatorial(10))
