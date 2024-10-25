5d806063ebbc64a489241430ad96312223713973
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-25 15:42:01 -0300

    Exercício 13

---
+++ 3-função/desafios/13.js
@@ -0,0 +1,20 @@
+/*
+13) ​Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado o número referente ao 
+dia. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch.
+*/
+
+function diaUtil(dia) {
+    switch (dia) {
+        case 1:
+            console.log('Hoje é domingo e estamos no final de semana.')
+            break
+        case 2: case 3: case 4: case 5: case 6:
+            console.log('Hoje é dia útil, VAI TRABALHAR!')
+            break
+        case 7:
+            console.log('Hoje é sábado e estamos no final de semana.')
+            break
+        }
+}
+
+diaUtil(7)
