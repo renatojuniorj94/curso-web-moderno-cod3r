cb3553fe30de40e84db338734ab71b994f9ec9ee
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-26 01:09:32 -0300

    Correção exercício 16

--- 3-função/desafios/16.js
+++ 3-função/desafios/16.js
@@ -29,3 +29,23 @@ console.log(calculadora(5, '%', 3))
 //Correto! :D
 //Maneira feita no curso:
 
+function calculadora (valor1, operacao, valor2) {
+    switch (operacao) {
+        case '+':
+            return valor1 + valor2
+        case '-':
+            return valor1 - valor2
+        case '*':
+            return valor1 * valor2
+        case '/':
+            return valor1 / valor2
+        default:
+            return 'Operação inválida'
+    }
+}
+
+console.log(calculadora(2, '+', 3));
+console.log(calculadora(2, '-', 3));
+console.log(calculadora(2, '*', 3));
+console.log(calculadora(2, '/', 3));
+console.log(calculadora(2, 'a', 3));
