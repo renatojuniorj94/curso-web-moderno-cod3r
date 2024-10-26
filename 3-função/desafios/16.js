089a8a2444262dc8a92463682db3f16887a8f767
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-26 01:04:40 -0300

    Exercício 16

---
+++ 3-função/desafios/16.js
@@ -0,0 +1,24 @@
+/* 
+16)​ Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebe 
+como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores 
+numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e 
+3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’.  Crie um caso default para operações inválidas.
+*/
+
+function calculadora(a, operador, b) {
+    switch(operador) {
+        case '+':
+            return a + b
+        case '-':
+            return a - b
+        case '*':
+            return a * b
+        case '/':
+            return a / b
+    }
+}
+
+console.log(calculadora(5, '+', 3))
+console.log(calculadora(5, '-', 3))
+console.log(calculadora(5, '*', 3))
+console.log(calculadora(5, '/', 3))
