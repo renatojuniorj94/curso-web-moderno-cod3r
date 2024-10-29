3d68b56db6e9ae44aa62c6bef1414d88e8b64913
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-29 01:09:34 -0300

    Correção exercício 26

--- 3-função/desafios/26.js
+++ 3-função/desafios/26.js
@@ -11,3 +11,16 @@ function exibirPares() {
 }
 
 exibirPares()
+
+//Correto! :D
+//Maneira feita no curso:
+
+function acharPares () {
+    for (let i = 1; i <= 100; i++){
+        if (i%2==0) {
+            console.log(i)
+        }
+    }
+}
+
+acharPares()
