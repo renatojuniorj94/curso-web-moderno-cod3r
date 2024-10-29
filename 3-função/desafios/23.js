b6acacec9f95c4bda78393b4da911dfd7b04650d
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-29 00:14:31 -0300

    Correção exercício 23

--- 3-função/desafios/23.js
+++ 3-função/desafios/23.js
@@ -20,3 +20,19 @@ function mediaAluno(codigo, n1, n2, n3) {
 console.log(mediaAluno(65, 2, 8, 3))
 console.log(mediaAluno(33, 9.2, 7, 8.5))
 console.log(mediaAluno(71, 5, 5, 4.99))
+
+//Correto! :D
+//Maneira feita no curso:
+
+function calcularNotaFinal(codAluno, nota1, nota2, nota3){
+    let notas = []
+    notas.push(nota1)
+    notas.push(nota2)
+    notas.push(nota3)
+    notas.sort((a,b) => a < b ? 1 : -1)
+
+    let mediaFinal = (notas[0] * 4 + notas[1] * 3 + notas[2] * 3)/10
+    console.log(`Código do Aluno: ${codAluno}. Notas: ${nota1}, ${nota2}, ${nota3}. ${mediaFinal < 5 ? 'Reprovado.' : 'Aprovado.'}`)
+}
+
+calcularNotaFinal(123, 2.8, 6, 3.5)
