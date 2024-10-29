7b7d585f62fb29b1aaf3c430f24b3137a3d4c0c6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-29 00:48:53 -0300

    Exercício 25

---
+++ 3-função/desafios/25.js
@@ -0,0 +1,13 @@
+/* 
+25) ​Escrever um programa para exibir os números de 1 até 50 na tela.
+*/
+
+function repetir(numeroDeRepetições) {
+    contador = 1
+    while (contador < numeroDeRepetições + 1) {
+        console.log(contador)
+        contador++
+    }
+}
+
+repetir(50)
