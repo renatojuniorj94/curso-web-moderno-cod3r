926ac3cb0dd692585a6ea463bc0f0e8c29a39790
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-31 17:36:23 -0300

    Correção exercício 31

--- 3-função/desafios/31.js
+++ 3-função/desafios/31.js
@@ -15,3 +15,20 @@ function qtdVetorNegativo(vetorArray) {
 
 vetor =  [10, 5, -7, 3, -1, 3, -11, -20, 6, 9]
 console.log(qtdVetorNegativo(vetor))
+
+//Correto! :D
+//Maneira feita no curso:
+
+function numerosNegativos (vetor) {
+    let qtdNegativos = 0
+    for(let i = 0; i < vetor.length; i++) {
+        if(vetor[i] < 0) {
+            qtdNegativos++
+        }
+    }
+    return qtdNegativos
+}
+
+vetor =  [10, 5, -7, 3, -1, 3, -11, -20, 6, 9]
+
+console.log(numerosNegativos(vetor))
