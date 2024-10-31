bcb21a5d45cd718b6ca4ab2e6f1761856c03c03f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-31 18:06:09 -0300

    Correção exercício 32

--- 3-função/desafios/32.js
+++ 3-função/desafios/32.js
@@ -12,3 +12,19 @@ function mediaAritmetica(vetorArray) {
 
 vetor = [7, 11, 4, 3, 22, 25, 1, 9]
 console.log(mediaAritmetica(vetor))
+
+//Correto! :D
+//Maneira feita no curso:
+
+function mediaVetor (vetor) {
+    let soma = 0
+    for(let i=0; i<vetor.length; i++) {
+        soma += vetor[i]
+    }
+
+    return soma/vetor.length
+}
+
+vetor = [1, 2, 3, 4, 5]
+
+console.log(mediaVetor(vetor))
