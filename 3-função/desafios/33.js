1642c040e11d12ec197e22a10f4f1867bcd80f54
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-31 18:48:48 -0300

    Exercício 33

---
+++ 3-função/desafios/33.js
@@ -0,0 +1,13 @@
+/* 
+33) ​Crie três vetores, chamados vetorInteiro, vetorString e vetorDouble. Cada um destes vetores deverá conter 
+quatro valores, sendo o primeiro com valores inteiros, o segundo com strings e o terceiro com valores decimais. 
+Declarados os vetores, utilize a função de união concat() de duas maneiras diferentes para unir os vetores, e 
+mostre o resultado no console. Todos os elementos do vetor resultado deverão aparecer no console.
+*/
+
+vetorInteiro = [1, 6, 7, 12]
+vetorString = ['2', '5', '8', '11']
+vetorDouble = [3.19, 4.8, 9.21, 10.04]
+
+todosOsVetores = vetorInteiro.concat(vetorString, vetorDouble).sort((a, b) => a - b)
+console.log(todosOsVetores)
