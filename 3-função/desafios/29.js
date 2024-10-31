8693935216191da678f9f9de7063c4af268be67b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-31 16:12:01 -0300

    Exercício 29

---
+++ 3-função/desafios/29.js
@@ -0,0 +1,19 @@
+/* 
+29) ​Utilizando a estrutura de repetição for, faça uma função que percorra um vetor e conte quantos números 
+deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos 
+deles estão fora do intervalo, escrevendo estas informações.
+*/
+
+function numerosNoIntervalor(array) {
+    contador = 0
+    for (i = 0; i < array.length; i++) {
+        if (array[i] >= 10 && array[i] <= 20) {
+            contador++
+        }
+    }
+    return `Temos ${contador} números no intervalo`
+}
+
+
+/* let vetor = [ 7, 8, 9, 10, 11, 12, 16, 17, 18, 19, 20, 21] */
+console.log(numerosNoIntervalor([2,8,10,11,14,19,20,28,31]))
