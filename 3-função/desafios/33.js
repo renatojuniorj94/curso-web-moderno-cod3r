01e16fe2fc0835636a180eb621bf7623a4c12878
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-31 19:26:51 -0300

    Correção exercício 33

--- 3-função/desafios/33.js
+++ 3-função/desafios/33.js
@@ -11,3 +11,21 @@ vetorDouble = [3.19, 4.8, 9.21, 10.04]
 
 todosOsVetores = vetorInteiro.concat(vetorString, vetorDouble).sort((a, b) => a - b)
 console.log(todosOsVetores)
+
+//Correto! :D
+//Maneira feita no curso:
+
+/* let vetorInteiro = [1, 2, 3, 4]
+let vetorString = ['Arthur', 'Christian', 'Yuri', 'Galdino']
+let vetorDouble = [1.1, 2.2, 3.3, 4.4]
+
+function concatenar (...args) {
+    resultado = []
+    for(let i = 0; i < arguments.length; i++){
+        resultado = resultado.concat(arguments[i])
+    }
+    return resultado;
+}
+
+console.log(concatenar(vetorInteiro, vetorDouble))
+console.log(concatenar(vetorDouble, vetorString)) */
