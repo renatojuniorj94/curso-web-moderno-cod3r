117271e0d15f1e7117c924635e708c7d485a76c2
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-10-31 22:24:49 -0300

    Exercício 35

---
+++ 3-função/desafios/35.js
@@ -0,0 +1,18 @@
+/* 
+35) ​Crie dois vetores chamados vetorPilha e vetorAdiciona. Inicialmente, o vetorPilha conterá cinco elementos 
+inteiros: [1, 2, 3, 4, 5]. Você deverá adicionar os valores contidos no vetorAdiciona [6, 7, 8, 9, 10] ao vetor pilha 
+e mostrá-los no console. É importante lembrar que o método Push retorna somente o tamanho do Vetor. Ao 
+final das operações imprima os vetores no console.
+*/
+
+vetorPilha = [1, 2, 3, 4, 5]
+vetorAdiciona = [6, 7, 8, 9, 10]
+
+function vetorPush(vetor1, vetor2) {
+    for (i = 0; i < vetor2.length; i++) {
+        vetor1.push(vetor2[i])
+    }
+    console.log(`Vetor adicionado: [${vetor1}]`)
+}
+
+vetorPush(vetorPilha, vetorAdiciona)
