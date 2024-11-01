497ed6776143ad043f534a4978cf2418cf8b8745
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-01 00:34:52 -0300

    Exercício 37

--- 3-função/desafios/37.js
+++ 3-função/desafios/37.js
@@ -1,3 +1,9 @@
+/* 
+37) ​Escreva duas funções, uma para progressão aritmética e uma para progressão geométrica que recebam 
+como parâmetros um número n (número de termo), ​a1​ (o primeiro termo) e ​r​ (a razão) e escreva os ​n​ termos , 
+bem como a soma dos elementos.
+*/
+
 function pA (n, a1, r) {
     for (let i = 0; i < n; i++) {
         console.log(a1 + r*i)
