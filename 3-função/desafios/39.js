b491c6b2efe4ed841b3a52cde1dcc5d442e4e315
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-02 02:00:00 -0300

    Correção exercício 39

--- 3-função/desafios/39.js
+++ 3-função/desafios/39.js
@@ -10,9 +10,9 @@ vetorB = [2, 4, 6, 8, 10]
 function trocaDeVetores(v1, v2) {
     if (v1.length === v2.length){
         for(c = 0; c < v1.length; c++) {
-            v1[c] = v1[c] + v2[c]
-            v2[c] = v1[c] - v2[c]
-            v1[c] = v1[c] - v2[c]
+            v1[c] = v1[c] + v2[c] //[ 3, 7, 11, 15, 19 ]
+            v2[c] = v1[c] - v2[c] //[ 1, 3, 5, 7, 9 ]
+            v1[c] = v1[c] - v2[c] //[ 2, 4, 6, 8, 10 ]
         }
         console.log(`Vetor 1 = [${v1}] \nVetor 2 = [${[v2]}]`)
     } else {
@@ -21,3 +21,26 @@ function trocaDeVetores(v1, v2) {
 }
 
 trocaDeVetores(vetorA, vetorB)
+
+//Correto! :D
+//Maneira feita no curso:
+
+/* function trocaValores(vetorA, vetorB) {
+    if (vetorA.length == vetorB.length) {
+        for(let i = 0; i < vetorA.length; i++){
+            vetorA[i] = vetorA[i] + vetorB[i]
+            vetorB[i] = vetorA[i] - vetorB[i]
+            vetorA[i] = vetorA[i] - vetorB[i]
+        }
+    } else {
+        return 'Vetores de tamanhos diferentes.'
+    }
+
+    console.log('Novo vetor A: ' + vetorA)
+    console.log('Novo vetor B: ' + vetorB)
+}
+
+let vetorA = [1, 2, 3]
+let vetorB = [4, 5, 6]
+
+trocaValores(vetorA, vetorB) */
