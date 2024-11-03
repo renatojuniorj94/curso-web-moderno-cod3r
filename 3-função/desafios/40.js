50cc18e157a84759fae6a6528cb9ba00deab3f5e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-03 01:26:59 -0300

    Exercício 40 (parte 2)

--- 3-função/desafios/40.js
+++ 3-função/desafios/40.js
@@ -7,20 +7,26 @@ conceito B e de 9,0 a 10,0 o conceito A.
 let notas = [10, 13, 9, 8.2, 2.5, 7.7, 6.8]
 
 function avaliacaoDeNotas(vetorNotas) {
-    for(c = 0; c < vetorNotas.length; c++) {
-        if (vetorNotas[c] <= 4.9) {
-            console.log(`Nota ${c} tem conceito D`)
-        }
-        if (vetorNotas[c] >= 5 && vetorNotas <= 6.9) {
-            console.log(`Nota ${c} tem conceito C`)
-        } 
-        if (vetorNotas[c] >= 7 && vetorNotas <= 8.9) {
-            console.log(`Nota ${c} tem conceito B`)
-        }
-        if (vetorNotas[c] >= 9 && vetorNotas <= 10) {
-            console.log(`Nota ${c} tem conceito A`)
+    let conceitos = []
+    for (let c = 0; c < vetorNotas.length; c++) {
+        if (vetorNotas[c] >= 0 && vetorNotas[c] <= 4.9) {
+            conceitos.push('D')
+            console.log(`Nota ${vetorNotas[c]} tem conceito ${conceitos[c]}`)
+        } else if (vetorNotas[c] >= 5 && vetorNotas[c] <= 6.9) {
+            conceitos.push('C')
+            console.log(`Nota ${vetorNotas[c]} tem conceito ${conceitos[c]}`)
+        } else if (vetorNotas[c] >= 7 && vetorNotas[c] <= 8.9) {
+            conceitos.push('B')
+            console.log(`Nota ${vetorNotas[c]} tem conceito ${conceitos[c]}`)
+        } else if (vetorNotas[c] >= 9 && vetorNotas[c] <= 10) {
+            conceitos.push('A')
+            console.log(`Nota ${vetorNotas[c]} tem conceito ${conceitos[c]}`)
+        } else {
+            conceitos.push('Nota inválida!')
         }
     }
+    console.log(conceitos)
+    /* return conceitos */
 }
 
 avaliacaoDeNotas(notas)
