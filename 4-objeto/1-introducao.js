b30b4ad85574965b51b35ecdbfeaffe93c039ed6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-04 17:37:01 -0300

    1 - Introdução (objetos)

---
+++ 4-objeto/1-introducao.js
@@ -0,0 +1,20 @@
+//Linguagem procedural
+processamento(valor1, valor2, valor3) //A função processa os valores
+
+//OO - Você tem dados, e esses dados manipulam as funções
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() //Invocando função atraves do dado e o foco passou a ser o objeto
+
+//Principios importantes:
+//1 - abstração
+//2 - encapsulamento > Mundanças internas que não dependem de adaptação do usuário
+//3- herança
+//4 - polimorfismo
