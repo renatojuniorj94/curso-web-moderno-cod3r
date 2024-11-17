6db77bbaeb63e71cf02a1af1a1cb35435f35bbc3
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-17 00:28:02 -0300

    5-map2.js

---
+++ 5-array/5-map2.js
@@ -0,0 +1,14 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+//Retornat um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json) //Convertendo para JSON
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
