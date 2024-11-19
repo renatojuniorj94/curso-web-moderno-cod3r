a6438055d15dc8bf1ab509f74809902afe2e2b75
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-18 23:00:19 -0300

    6-filter1.js (parte 2)

--- 5-array/6-filter1.js
+++ 5-array/6-filter1.js
@@ -10,3 +10,16 @@ const produtos = [
 console.log(produtos.filter(function(p) {
     return p.preco > 2500
 }))
+
+
+//Desafio: Extrair array com preço maior que 500 e fragil === true
+console.log(produtos.filter(function(prod) {
+    return prod.preco > 500 && prod.fragil === true
+}))
+
+//Resolução do desafio:
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter(caro).filter(fragil))
