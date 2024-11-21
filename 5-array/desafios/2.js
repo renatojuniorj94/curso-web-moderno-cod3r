8291b2b7c57d8f302fe2387c06147198bb628baa
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-21 00:09:19 -0300

    Enunciado desafio 2.js

---
+++ 5-array/desafios/2.js
@@ -0,0 +1,7 @@
+/*
+Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+OBS: Considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e desconsidere também dias decorridos desde o último aniversário.
+Exemplos:
+converterIdadeEmAnosParaDias(25) // retornará 9125
+converterIdadeEmAnosParaDias(70) // retornará 25550
+*/
