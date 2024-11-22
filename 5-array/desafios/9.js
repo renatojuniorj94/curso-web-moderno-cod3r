2a3976fa552936a584a63b34828e78f88e97b4aa
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-22 19:01:18 -0300

    Resolução desafio 9

--- 5-array/desafios/9.js
+++ 5-array/desafios/9.js
@@ -8,11 +8,29 @@ repetir(7, 3) // retornará [7, 7, 7]
 
 function repetir(elemento, repetições) {
     let resultado = []
-    for(let i = 1; i <= repetições; i++){
-    resultado.push(elemento)
+    for (let i = 1; i <= repetições; i++) {
+        resultado.push(elemento)
     }
     return resultado
 }
 
 console.log(repetir("código", 2))
 console.log(repetir(7, 3))
+
+//Correto! :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function repetir(item, quantidade) {
+    let resultado = []
+
+    for (let i = 0; i < quantidade; i++)
+        resultado.push(item)
+
+    return resultado
+}
+
+//Resolução 2:
+function repetir(item, quantidade) {
+    return Array(quantidade).fill(item)
+}
