ba6d72351b220c325b47086c80d9a9206b30f8fb
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-22 22:25:24 -0300

    Resolução desafio 12

---
+++ 5-array/desafios/12.js
@@ -0,0 +1,48 @@
+/* 
+Quando temos um objeto e manipulamos seus atributos, adicionando, atualizando ou removendo-os, estamos 
+apenas modificando-o, mas, em essência, o objeto continua o mesmo, ou seja a sua referência de memória é a 
+mesma.
+Num projeto que você está trabalhando, você foi designado a refatorar diversas funções para que façam cópias 
+de objetos e manipulem os dados dessas cópias, com o intuito de evitar efeitos indesejáveis em algumas 
+situações devido a referências a objetos. Abaixo, está a descrição de uma dessas funções. 
+Você escreverá uma função que recebe um objeto como primeiro parâmetro e, como segundo parâmetro, o nome 
+de uma propriedade contida nesse objeto. Em seguida, retorne uma cópia desse objeto sem a propriedade 
+especificada no segundo parâmetro.
+Exemplos:
+removerPropriedade({a: 1, b: 2}, "a") // retornará {b: 2} 
+removerPropriedade({ 
+  id: 20, 
+  nome: "caneta", 
+  descricao: "Não preenchido" 
+}, "descricao") // retornará {id: 20, nome: "caneta"}
+💡A fim de testar se o objeto retornado não é o mesmo que foi passado como parâmetro para a função 
+removerPropriedade, você poderá usar a função Object.is(), por exemplo:  
+Object.is(removerPropriedade(objeto, "descricao"), objeto)  
+Retornará false se o objeto não for o mesmo.
+*/
+
+let pessoa = {
+    nome: 'Renato',
+    idade: 30,
+    nacionalidade: 'Brasileiro'
+}
+
+//Resolução 1:
+function removerPropriedade(objeto, nomeDaPropriedade) {
+    const copia = Object.assign({}, objeto)
+    delete copia[nomeDaPropriedade]
+
+    return copia
+}
+
+//Resolução 2:
+function removerPropriedade(objeto, nomeDaPropriedade) {
+    const copia = { ...objeto }
+    delete copia[nomeDaPropriedade]
+
+    return copia
+}
+
+console.log(removerPropriedade(pessoa, 'nacionalidade'))
+console.log(Object.is(removerPropriedade(pessoa, "nacionalidade"), pessoa))
+console.log(pessoa)
