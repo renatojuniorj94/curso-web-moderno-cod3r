bb0eff1c55634bb0c399306a12796fd14891c480
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-25 00:22:05 -0300

    Resolução desafio 13

--- 5-array/desafios/13.js
+++ 5-array/desafios/13.js
@@ -7,6 +7,21 @@ filtrarNumeros(["Javascript", 1, "3", "Web", 20]) // retornará [1, 20]
 filtrarNumeros(["a", "c"]) // retornará []
 */
 
-function filtrarNumero([]) {
-    
+//Resolução 1:
+function filtrarNumeros(array) {
+    const resultado = []
+
+    for (let item of array)
+        if (typeof item === "number")
+            resultado.push(item)
+
+    return resultado
 }
+
+//Resolução 2:
+function filtrarNumeros(array) {
+    return array.filter(item => typeof item === "number")
+}
+
+console.log(filtrarNumeros(["Javascript", 1, "3", "Web", 20]))
+console.log(filtrarNumeros(["a", "c"]))
