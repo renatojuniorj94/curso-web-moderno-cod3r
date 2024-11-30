d2988de82e6c510c95dacae86b04b65334debbf0
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-30 12:26:46 -0300

    Resolução desafio 15

--- 5-array/desafios/15.js
+++ 5-array/desafios/15.js
@@ -23,3 +23,30 @@ function receberSomenteOsParesDeIndicesPares(array) {
 console.log(receberSomenteOsParesDeIndicesPares(num))
 console.log(receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]))
 console.log(receberSomenteOsParesDeIndicesPares([10, 70, 22, 43]))
+
+//Correto! :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function receberSomenteOsParesDeIndicesPares(numeros) {
+    let resultado = []
+
+    for (let i = 0; i < numeros.length; i += 2) {
+        const numeroPar = numeros[i] % 2 === 0
+
+        if (numeroPar)
+            resultado.push(numeros[i])
+    }
+
+    return resultado
+}
+
+//Resolução 2:
+function receberSomenteOsParesDeIndicesPares(numeros) {
+    return numeros.filter((numero, indice) => {
+        const numeroPar = numero % 2 === 0
+        const indicePar = indice % 2 === 0
+
+        return numeroPar && indicePar
+    })
+}
