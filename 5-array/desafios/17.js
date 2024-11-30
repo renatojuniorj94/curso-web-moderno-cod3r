a9aacbb63df33b452bd48950430675b4d9afdfed
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-11-30 13:35:41 -0300

    Resolução desafio 17

--- 5-array/desafios/17.js
+++ 5-array/desafios/17.js
@@ -7,7 +7,7 @@ somarNumeros([15, 15, 15, 15]) // retornará 60
 
 function somarNumeros(array) {
     let resultado = 0
-    for(i = 0; i < array.length; i++) {
+    for (i = 0; i < array.length; i++) {
         resultado += array[i]
     }
     return resultado
@@ -15,3 +15,28 @@ function somarNumeros(array) {
 
 console.log(somarNumeros([10, 10, 10]))
 console.log(somarNumeros([15, 15, 15, 15]))
+
+//Correto! :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function somarNumeros(numeros) {
+    const quantidadeDeNumeros = numeros.length
+
+    return (quantidadeDeNumeros === 0) ? 0 : numeros[0] + somarNumeros(numeros.slice(1))
+}
+
+//Resolução 2:
+function somarNumeros(numeros) {
+    let soma = 0
+    numeros.forEach(numero => soma += numero)
+
+    return soma
+}
+
+//Resolução 3:
+function somarNumeros(numeros) {
+    const soma = numeros.reduce((acumulador, numeroAtual) => acumulador + numeroAtual, 0)
+
+    return soma
+}
