1b0adecca08a8cbc019ea3f4b2c205b8413d2c33
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-10 18:20:41 -0300

    Resolução desafio 19

--- 5-array/desafios/19.js
+++ 5-array/desafios/19.js
@@ -21,3 +21,26 @@ function calcularMedia(array) {
 
 console.log(calcularMedia([0, 10]))
 console.log(calcularMedia([1, 2, 3, 4, 5]))
+
+//Correto! :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function calcularMedia(numeros) {
+    const quantidadeDeNumeros = numeros.length
+    let somaTotal = 0
+
+    for (numero of numeros) {
+        somaTotal += numero
+    }
+
+    return somaTotal / quantidadeDeNumeros
+}
+
+//Resolução 2:
+function calcularMedia(numeros) {
+    const quantidadeDeNumeros = numeros.length
+    const somaTotal = numeros.reduce((numeroA, numeroB) => numeroA + numeroB)
+
+    return somaTotal / quantidadeDeNumeros
+}
