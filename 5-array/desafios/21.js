d8108a329e7ade22d4e43d79ec0f601e2054b1e7
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-10 19:15:35 -0300

    Resolução desafio 21

--- 5-array/desafios/21.js
+++ 5-array/desafios/21.js
@@ -7,7 +7,7 @@ menorNumero([5, -15, 50, 3]) // retornará -15
 
 function menorNumero(array) {
     let menorNum = array[0]
-    for(let i in array) {
+    for (let i in array) {
         if (array[i] < menorNum) {
             menorNum = array[i]
         }
@@ -17,3 +17,27 @@ function menorNumero(array) {
 
 console.log(menorNumero([10, 5, 35, 65]))
 console.log(menorNumero([5, -15, 50, 3]))
+
+//Correto! :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function menorNumero(numeros) {
+    let menor = numeros[0]
+
+    for (let i in numeros)
+        if (numeros[i] < menor)
+            menor = numeros[i]
+
+    return menor
+}
+
+//Resolução 2:
+function menorNumero(numeros) {
+    return numeros.reduce((anterior, atual) => anterior < atual ? anterior : atual)
+}
+
+//Resolução 3:
+function menorNumero(numeros) {
+    return Math.min(...numeros);
+}
