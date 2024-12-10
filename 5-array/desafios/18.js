98f27d694b79707a2fb2dd2afc4f6c8b429fff51
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-10 00:52:35 -0300

    Resolução desafio 18

--- 5-array/desafios/18.js
+++ 5-array/desafios/18.js
@@ -32,3 +32,28 @@ console.log(despesasTotais([
   {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99}, 
   {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90} 
 ]))
+
+//Correto! :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function despesasTotais(itens) { 
+  var total = 0 
+   
+  for (let item of itens) 
+    total += item.preco 
+   
+  return total 
+}
+
+//Resolução 2:
+function despesasTotais(itens) { 
+  return itens 
+    .map(item => item.preco) 
+    .reduce((acumulador, valorAtual) => acumulador + valorAtual) 
+}
+
+//Resolução 3:
+function despesasTotais(itens) { 
+  return itens.reduce((acumulador, valorAtual) => acumulador + valorAtual.preco, 0) 
+}
