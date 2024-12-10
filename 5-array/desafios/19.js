a6ada5b2f71087265cce3d62674fd1e036c1b727
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-10 01:19:42 -0300

    Desafio 19

---
+++ 5-array/desafios/19.js
@@ -0,0 +1,23 @@
+/* 
+Numa aplicação Web de investimento financeiro da qual você faz parte da equipe de desenvolvimento, pretende-
+se adicionar a funcionalidade de calcular a média de um conjunto de números informados pelo usuário.
+Com o intuito de realizar esse cálculo, crie uma função que receba um array com uma quantidade indeterminada 
+de números e retorne a média simples desses números.
+💡A média simples é o resultado da soma de todos os números dividido pela quantidade de números.
+Exemplos:
+calcularMedia([0, 10]) // retornará 5  
+calcularMedia([1, 2, 3, 4, 5]) // retornará 3
+*/
+
+function calcularMedia(array) {
+    let total = 0
+    let newArray = array
+    for (let i = 0; i < array.length; i++) {
+        total += newArray[i]
+    }
+    let resultado = total / array.length
+    return resultado
+}
+
+console.log(calcularMedia([0, 10]))
+console.log(calcularMedia([1, 2, 3, 4, 5]))
