9855a795baffd3e7e797294b508c4c0b38948fac
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-13 00:14:30 -0300

    Enunciado desafio 26

---
+++ 5-array/desafios/26.js
@@ -0,0 +1,7 @@
+/* 
+Desenvolva uma função que receba uma frase como parâmetro e retorne essa string somente com as consoantes, 
+ou seja, sem as vogais.
+Exemplos:
+removerVogais("Cod3r") // retornará "Cd3r" 
+removerVogais("Fundamentos") // retornará "Fndmnts"
+*/
