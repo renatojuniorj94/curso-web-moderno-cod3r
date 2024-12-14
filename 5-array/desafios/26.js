4077ab6ed26868bff266cc10c5f2dad5644ca70e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-14 20:56:11 -0300

    Resolução desafio 26

--- 5-array/desafios/26.js
+++ 5-array/desafios/26.js
@@ -17,3 +17,38 @@ function removerVogais(string) {
 console.log(removerVogais("Cod3r"))
 console.log(removerVogais("Fundamentos"))
 console.log(removerVogais("Renato"))
+
+//Correto! :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function removerVogais(frase) { 
+  const vogais = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"] 
+   
+  vogais.forEach( vogal => frase = frase.replace(vogal, '') ) 
+ 
+  return frase 
+}
+
+//Resolução 2:
+function removerVogais(frase) { 
+  return frase.replace(/[aeiou]/ig, '') 
+}
+
+/* 
+2.1. Primeiro argumento: /[aeiou]/ig
+Isso é uma expressão regular (regex). Vamos desmembrar:
+
+/[aeiou]/: Os colchetes indicam um conjunto de caracteres. Aqui, está dizendo para encontrar qualquer letra que seja uma vogal (a, e, i, o, u).
+i: É uma flag que torna a busca case-insensitive, ou seja, ela encontra tanto maiúsculas quanto minúsculas (ex.: "A", "e").
+g: É a flag global, que faz a busca se aplicar a todas as ocorrências na string, não apenas à primeira.
+2.2. Segundo argumento: ''
+É a string que substituirá as correspondências encontradas pela regex.
+Aqui, é uma string vazia (''), o que significa que as vogais encontradas serão removidas.
+
+As barras / / em JavaScript são usadas para definir expressões regulares (regex), que são padrões utilizados para realizar buscas, correspondências ou substituições em strings.
+
+As barras / / delimitam o início e o fim da expressão regular.
+Entre as barras, você especifica o padrão que deseja buscar.
+Após as barras, você pode incluir flags que modificam o comportamento da busca.
+*/
