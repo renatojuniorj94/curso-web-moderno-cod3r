062bb8605471d308eccdc83ae6dd866e56f96a2e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-14 23:30:05 -0300

    Resolução desafio 27

--- 5-array/desafios/27.js
+++ 5-array/desafios/27.js
@@ -5,3 +5,27 @@ seguir:
 Exemplo:
 inverter({ a: 1, b: 2, c: 3}) // retornará { 1: "a", 2: "b", 3: "c"}
 */
+
+//Resolução 1:
+function inverter(objeto) { 
+  const objetoInvertido = {} 
+ 
+  Object.entries(objeto).forEach( parChaveValor => { 
+    const chave = 0, 
+          valor = 1 
+ 
+    objetoInvertido[ parChaveValor[valor] ] = parChaveValor[chave] 
+  }) 
+ 
+  return objetoInvertido 
+}
+
+//Resolução 2:
+function inverter(objeto) { 
+  const paresDeChaveValorInvertidos = Object.entries(objeto) 
+          .map( parChaveValor => parChaveValor.reverse() ) 
+ 
+  return Object.fromEntries(paresDeChaveValorInvertidos) 
+}
+
+console.log(inverter({ a: 1, b: 2, c: 3}))
