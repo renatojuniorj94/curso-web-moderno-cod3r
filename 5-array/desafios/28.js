5e96c27d2c60ac4ef3d1a71cc5e74df80f53676e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-15 00:19:34 -0300

    Resolução desafio 28

--- 5-array/desafios/28.js
+++ 5-array/desafios/28.js
@@ -21,3 +21,28 @@ function filtrarPorQuantidadeDeDigitos(array, quantosDigitos) {
 
 console.log(filtrarPorQuantidadeDeDigitos([38, 2, 365, 10, 125, 11], 2))
 console.log(filtrarPorQuantidadeDeDigitos([5, 9, 1, 125, 11], 1))
+
+//Correto :D
+//Outra maneira de fazer:
+
+//Resolução 1:
+function filtrarPorQuantidadeDeDigitos(numeros, quantidadeDesejada) { 
+  let resultado = [] 
+  for(numero of numeros){ 
+    const quantidadeDeDigitos = String(numero).length 
+ 
+    if(quantidadeDeDigitos === quantidadeDesejada) 
+      resultado.push(numero) 
+  } 
+ 
+  return resultado 
+}
+
+//Resolução 2:
+function filtrarPorQuantidadeDeDigitos(numeros, quantidadeDesejada) { 
+  return numeros.filter(numero => { 
+    const quantidadeDeDigitos = String(numero).length 
+ 
+    return quantidadeDeDigitos === quantidadeDesejada 
+  }) 
+}
