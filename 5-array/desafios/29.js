90d4749f4e3014ae9a605bfc8b5f2f81da978c24
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-16 00:45:38 -0300

    Enunciado desafio 29

---
+++ 5-array/desafios/29.js
@@ -0,0 +1,6 @@
+/* 
+Crie uma função que recebe um array de números e retorna o segundo maior número presente nesse array.
+Exemplos:
+segundoMaior([12, 16, 1, 5]) // retornará 12 
+segundoMaior([8, 4, 5, 6]) // retornará 6
+*/
