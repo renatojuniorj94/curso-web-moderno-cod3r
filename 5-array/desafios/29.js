355a43ebc3b4705663816f82a4aa591709cd30ef
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-23 01:11:47 -0300

    Desafio 29

--- 5-array/desafios/29.js
+++ 5-array/desafios/29.js
@@ -4,3 +4,26 @@ Exemplos:
 segundoMaior([12, 16, 1, 5]) // retornará 12 
 segundoMaior([8, 4, 5, 6]) // retornará 6
 */
+
+function segundoMaior(numeros) { 
+    let indiceDoMaior = 0 
+    let segundoMaior 
+     
+    numeros.forEach( (numero, indice) => { 
+      if( numero > numeros[indiceDoMaior] ) 
+        indiceDoMaior = indice 
+    }) 
+   
+    numeros.splice(indiceDoMaior, 1) 
+    segundoMaior = numeros[0] 
+     
+    numeros.forEach(numero => { 
+      if(numero > segundoMaior) 
+        segundoMaior = numero 
+    }) 
+     
+    return segundoMaior 
+  }
+
+console.log(segundoMaior([12, 16, 1, 5]))
+console.log(segundoMaior([8, 4, 5, 6]))
