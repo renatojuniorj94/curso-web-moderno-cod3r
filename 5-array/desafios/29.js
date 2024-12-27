39c6fbe0c3527fdbcfc60f9b1f940767802fb04c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-26 22:25:18 -0300

    Resolução desafio 29

--- 5-array/desafios/29.js
+++ 5-array/desafios/29.js
@@ -5,27 +5,43 @@ segundoMaior([12, 16, 1, 5]) // retornará 12
 segundoMaior([8, 4, 5, 6]) // retornará 6
 */
 
-function segundoMaior(numeros) { 
-    let indiceDoMaior = 0 
-    let segundoMaior 
-     
-    numeros.forEach( (numero, indice) => { 
-      if( numero > numeros[indiceDoMaior] ) 
-        indiceDoMaior = indice 
-    }) 
-   
-    numeros.splice(indiceDoMaior, 1) 
-    segundoMaior = numeros[0] 
-     
-    numeros.forEach(numero => { 
-      if(numero > segundoMaior) 
-        segundoMaior = numero 
-    }) 
-     
-    return segundoMaior 
-  }
+//Resolução 1:
+function segundoMaior(numeros) {
+  let indiceDoMaior = 0
+  let segundoMaior
+
+  numeros.forEach((numero, indice) => {
+    if (numero > numeros[indiceDoMaior])
+      indiceDoMaior = indice
+  })
+
+  numeros.splice(indiceDoMaior, 1)
+  segundoMaior = numeros[0]
+
+  numeros.forEach(numero => {
+    if (numero > segundoMaior)
+      segundoMaior = numero
+  })
+
+  return segundoMaior
+}
+
+//Resolução 2:
+function segundoMaior(numeros) {
+  const maiorNumero = Math.max(...numeros)
+  numeros = numeros.filter(numero => numero != maiorNumero)
+  const segundoMaior = Math.max(...numeros)
+
+  return segundoMaior
+}
+
+//Resolução 3:
+function segundoMaior(numeros) {
+  const numerosOrdenados = numeros.sort((numeroA, numeroB) => numeroB - numeroA)
+  const segundoMaior = numerosOrdenados[1]
+
+  return segundoMaior
+}
 
 console.log(segundoMaior([12, 16, 1, 5]))
-console.log(segundoMaior([8, 4, 5, 6]))
-console.log(segundoMaior([8, 4, 5, 6]))
 console.log(segundoMaior([8, 4, 5, 6]))
