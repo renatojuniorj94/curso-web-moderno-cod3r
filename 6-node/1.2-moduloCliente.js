ff360a0d2cfb593537d712ba5f22116f73e69fa5
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-12-31 01:15:50 -0300

    usandoModulosTerceiros.js

--- 6-node/1.2-moduloCliente.js
+++ 6-node/1.2-moduloCliente.js
@@ -10,4 +10,3 @@ console.log(moduloA)
 console.log(moduloB.bomDia)
 console.log(moduloB.boaNoite())
 console.log(moduloB)
-
---
+++ 6-node/usandoModulosTerceiros.js
@@ -0,0 +1,6 @@
+const _ = require('lodash')
+setInterval(() => console.log(_.random(1, 1000)), 2000)
+
+/* for(i = 0; i < 6; i++) {
+    console.log(`${i+1}º`,_.random(1, 61))
+} */
