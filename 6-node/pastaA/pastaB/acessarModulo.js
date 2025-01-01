6fd4dc0415d1eed831409c6a781d9391d6d94796
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-01-01 13:14:35 -0300

    Sistema de módulos: Require (parte 2)

--- 6-node/pastaA/pastaB/acessarModulo.js
+++ 6-node/pastaA/pastaB/acessarModulo.js
@@ -1,3 +1,5 @@
 const moduloA = require('../../1-moduloA') //Se coloca em letra minuscula ('../../1-moduloa') não vai funcionar no linux
 //Outra maneira de passar o caminho do arquivo, é clicando com botão direiro em cima do arquivo > copy path (copiar caminho). Mas não é a melhor pratica.
 console.log(moduloA)
+
+//Paramos a aula com 4:36
