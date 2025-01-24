c55c459aff889ca0daa3a42df091d1c9542ea363
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-01-24 01:50:44 -0300

    Lendo arquivos

---
+++ 6-node/10.2-arquivoLeitura.js
@@ -0,0 +1,21 @@
+const fs = require('fs') //fs = file system (módulo nativo do node)
+
+const caminho = __dirname + '/10-arquivo.json'
+
+//sincrono...
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+//assincrono...
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}:${config.db.port}`)
+})
+
+const config = require('./10-arquivo.json')
+console.log(config.db)
+
+fs.readdir(__dirname, (err, arquivos) => {
+    console.log('Conteúdo da pasta...')
+    console.log(arquivos)
+})
