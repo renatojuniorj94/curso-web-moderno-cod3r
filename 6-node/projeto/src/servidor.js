ee2c26eff8127d148609595acf76f78151015df1
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-02-01 04:33:07 -0300

    Projeto API com express #03

--- 6-node/projeto/src/servidor.js
+++ 6-node/projeto/src/servidor.js
@@ -2,28 +2,45 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
-const bancoDeDados = require('./bandoDeDados')
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/* app.get('/produtos', (req, res, next) => {
     console.log('Middleware 1...')
     next()
-})
+}) */
 
 app.get('/produtos', (req, res, next) => {
     //Antes > res.send({ nome: 'Notebook', preco: 123.45}) //O metódo send vai converter o objeto para JSON
-    res.send(bancoDeDados.getProdutos(req.params.id))
+    res.send(bancoDeDados.getProdutos())
 })
 
 app.get('/produtos/:id', (req, res, next) => {
-    res.send(bancoDeDados.getProduto())
+    res.send(bancoDeDados.getProduto(req.params.id))
 })
 
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProdutos(req.params.id)
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
