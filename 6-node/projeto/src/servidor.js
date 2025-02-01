caf58d06a30b6e0951b787d91908177dced0396b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-01-31 21:45:36 -0300

    Projeto API com express #01

--- 6-node/projeto/src/servidor.js
+++ 6-node/projeto/src/servidor.js
@@ -1,3 +1,17 @@
-/* 
-server.js
-*/
+const porta = 3003
+
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, next) => {
+    console.log('Middleware 1...')
+    next()
+})
+
+app.get('/produtos', (req, res, next) => {
+    res.send({ nome: 'Notebook', preco: 123.45}) //O metódo send vai converter o objeto para JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
