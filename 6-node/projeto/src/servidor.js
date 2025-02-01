f3a621435bf1dccc229ac5cd9c107cf3fc9c6f24
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-02-01 03:15:54 -0300

    Projeto API com express #02

--- 6-node/projeto/src/servidor.js
+++ 6-node/projeto/src/servidor.js
@@ -2,6 +2,7 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bandoDeDados')
 
 app.get('/produtos', (req, res, next) => {
     console.log('Middleware 1...')
@@ -9,7 +10,20 @@ app.get('/produtos', (req, res, next) => {
 })
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45}) //O metódo send vai converter o objeto para JSON
+    //Antes > res.send({ nome: 'Notebook', preco: 123.45}) //O metódo send vai converter o objeto para JSON
+    res.send(bancoDeDados.getProdutos(req.params.id))
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto())
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
