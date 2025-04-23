82b9841b9de7857fa23139c0cd6c2d2ad42e18e1
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-04-23 00:07:58 -0300

    Criando função parDeBarreiras()

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -15,6 +15,33 @@ function barreira(reversa = false) {
     this.setAltura = altura => corpo.style.height = `${altura}px`
 }
 
-const b = new barreira(true)
+/* const b = new barreira(true)
 b.setAltura(200)
+document.querySelector('[wm-flappy').appendChild(b.elemento) */
+
+function parDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new barreira(true)
+    this.inferior = new barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clienteWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+const b = new parDeBarreiras(700, 200, 800)
 document.querySelector('[wm-flappy').appendChild(b.elemento)
