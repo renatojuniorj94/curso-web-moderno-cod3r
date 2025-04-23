0a63f1b398973792563debfc5c488780339e43f7
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-04-22 23:50:47 -0300

    Criando função construtora barreira ()

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -4,3 +4,17 @@ function novoElemento(tagName, className) {
     return elem
 }
 
+function barreira(reversa = false) {
+    this.elemento = novoElemento('div', 'barreira')
+
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`
+}
+
+const b = new barreira(true)
+b.setAltura(200)
+document.querySelector('[wm-flappy').appendChild(b.elemento)
