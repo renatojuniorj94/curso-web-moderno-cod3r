91b60ded5041262d87801116b2382bc01c314391
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-05-13 16:30:44 -0300

    Implementando server.js #02

--- 11-ajax/server.js
+++ 11-ajax/server.js
@@ -5,5 +5,5 @@ const app = express()
 app.use(express.static('.'))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok')) //Função middleware
+app.get('/teste', (req, res) => res.send(new Date)) //Função middleware
 app.listen(8080, () => console.log('Executando...'))
