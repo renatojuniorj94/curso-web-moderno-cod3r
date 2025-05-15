971afcab2b765c692ba3eab0632490a327865025
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2025-05-15 11:38:24 -0300

    Update server.js

--- 11-ajax/server.js
+++ 11-ajax/server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('.'))
-app.use(bodyParser.json())
-
+//As duas linhas abaixo, por enquanto não estão sendo usadas.
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/teste', (req, res) => res.send(new Date)) //Função middleware
+
 app.listen(8080, () => console.log('Executando...'))
